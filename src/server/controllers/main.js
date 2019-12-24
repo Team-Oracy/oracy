1e0c1fa5c42bcc8b12738a17f72d99109502d557
Damir Kotorić <damirkotoric@gmail.com>
2019-12-24 16:09:51 +1100

    caching featured audiobooks for faster development

--- src/server/controllers/main.js
+++ src/server/controllers/main.js
@@ -1,27 +1,43 @@
-const axios = require('axios');
+const axios = require('axios')
+const fs = require('fs')
 const featuredAudiobookIds = require('../data/featuredAudiobookIds.json')
+const cachedFilePath = __dirname + '/../data/_cachedFeaturedAudiobooks.json'
 let featuredAudiobooks = []
 
 async function fetchAudiobooks() {
-  try {
-    const fetchedAudiobooks = []
-    const promises = featuredAudiobookIds.map(async featuredAudiobookId => {
-      const response = await axios('https://archive.org/metadata/'+featuredAudiobookId)
-      const cleanedUpDescription = response.data.metadata.description.replace(/<\/?("[^"]*"|'[^']*'|[^>])*(>|$)/g, "")
-      const audiobook = {
-        title: response.data.metadata.title,
-        description: cleanedUpDescription.substr(0, 256),
-        author: response.data.metadata.creator,
-        coverImageSrc: "https://" + response.data.d1 + response.data.dir + "/" + response.data.files[response.data.files.findIndex(file => file.format === "JPEG")].name,
-        audioTracks: fetchTracks(response.data)
-      }
-      fetchedAudiobooks.push(audiobook)
-    })
-    await Promise.all(promises)
-    featuredAudiobooks = fetchedAudiobooks
+  if(fs.existsSync(cachedFilePath)) {
+    // Cached file exists, use that.
+    let cachedFile
+    try {
+      cachedFile = fs.readFileSync(__dirname + '/../data/_cachedFeaturedAudiobooks.json', 'utf8')
+    } catch (err) {
+      console.log(err)
+    }
+    featuredAudiobooks = JSON.parse(cachedFile)
     return
-  } catch (e) {
-    console.error(e)
+  } else {
+    // Cached file does not exist.
+    try {
+      const fetchedAudiobooks = []
+      const promises = featuredAudiobookIds.map(async featuredAudiobookId => {
+        const response = await axios('https://archive.org/metadata/'+featuredAudiobookId)
+        const cleanedUpDescription = response.data.metadata.description.replace(/<\/?("[^"]*"|'[^']*'|[^>])*(>|$)/g, "")
+        const audiobook = {
+          title: response.data.metadata.title,
+          description: cleanedUpDescription.substr(0, 256),
+          author: response.data.metadata.creator,
+          coverImageSrc: "https://" + response.data.d1 + response.data.dir + "/" + response.data.files[response.data.files.findIndex(file => file.format === "JPEG")].name,
+          audioTracks: fetchTracks(response.data)
+        }
+        fetchedAudiobooks.push(audiobook)
+      })
+      await Promise.all(promises)
+      fs.writeFileSync(__dirname + '/../data/_cachedFeaturedAudiobooks.json', JSON.stringify(fetchedAudiobooks))
+      featuredAudiobooks = fetchedAudiobooks
+      return
+    } catch (e) {
+      console.error(e)
+    }
   }
 }
 fetchAudiobooks()
